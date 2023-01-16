0f6be72b3089cfd7339d1501730ca8267749b077
Diego Andrés Parra Espinosa <113558619+DiegoParraE@users.noreply.github.com>
2023-01-16 00:05:41 -0500

    actualizacion link y css

--- bit06/javascript/main.js
+++
@@ -1,7 +0,0 @@
-let form = document.getElementById('form');
-let title = document.getElementById('title');
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    title.innerHTML = `¡¡Hola ${form.name.value}!!`;
-});
